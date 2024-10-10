f33582b0770d974977f863fd5ed2b6e4745dd32f
hali278 <hlmhi.imah0@gmail.com>
2024-10-10 23:02:33 +0700

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buka = new System.Windows.Forms.Button();
+            this.simpan = new System.Windows.Forms.Button();
+            this.hapus = new System.Windows.Forms.Button();
+            this.cari = new System.Windows.Forms.Button();
+            this.text_cari = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.text_edit = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buka
+            // 
+            this.buka.Location = new System.Drawing.Point(38, 27);
+            this.buka.Name = "buka";
+            this.buka.Size = new System.Drawing.Size(75, 23);
+            this.buka.TabIndex = 0;
+            this.buka.Text = "Buka";
+            this.buka.UseVisualStyleBackColor = true;
+            this.buka.Click += new System.EventHandler(this.buka_Click);
+            // 
+            // simpan
+            // 
+            this.simpan.Location = new System.Drawing.Point(119, 27);
+            this.simpan.Name = "simpan";
+            this.simpan.Size = new System.Drawing.Size(75, 23);
+            this.simpan.TabIndex = 1;
+            this.simpan.Text = "Simpan";
+            this.simpan.UseVisualStyleBackColor = true;
+            this.simpan.Click += new System.EventHandler(this.simpan_Click);
+            // 
+            // hapus
+            // 
+            this.hapus.Location = new System.Drawing.Point(200, 27);
+            this.hapus.Name = "hapus";
+            this.hapus.Size = new System.Drawing.Size(75, 23);
+            this.hapus.TabIndex = 2;
+            this.hapus.Text = "Hapus";
+            this.hapus.UseVisualStyleBackColor = true;
+            this.hapus.Click += new System.EventHandler(this.hapus_Click);
+            // 
+            // cari
+            // 
+            this.cari.Location = new System.Drawing.Point(442, 28);
+            this.cari.Name = "cari";
+            this.cari.Size = new System.Drawing.Size(75, 23);
+            this.cari.TabIndex = 3;
+            this.cari.Text = "Cari";
+            this.cari.UseVisualStyleBackColor = true;
+            this.cari.Click += new System.EventHandler(this.cari_Click);
+            // 
+            // text_cari
+            // 
+            this.text_cari.Location = new System.Drawing.Point(333, 30);
+            this.text_cari.Name = "text_cari";
+            this.text_cari.Size = new System.Drawing.Size(100, 20);
+            this.text_cari.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Cari :";
+            // 
+            // text_edit
+            // 
+            this.text_edit.Location = new System.Drawing.Point(38, 72);
+            this.text_edit.Multiline = true;
+            this.text_edit.Name = "text_edit";
+            this.text_edit.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.text_edit.Size = new System.Drawing.Size(479, 289);
+            this.text_edit.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.text_edit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_cari);
+            this.Controls.Add(this.cari);
+            this.Controls.Add(this.hapus);
+            this.Controls.Add(this.simpan);
+            this.Controls.Add(this.buka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buka;
+        private System.Windows.Forms.Button simpan;
+        private System.Windows.Forms.Button hapus;
+        private System.Windows.Forms.Button cari;
+        private System.Windows.Forms.TextBox text_cari;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_edit;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private string currentFilePath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buka_Click(object sender, EventArgs e)
+        {
+            // Menggunakan FolderBrowserDialog untuk membuka folder
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFolder = folderBrowserDialog.SelectedPath;
+
+                // Mendapatkan semua file di folder yang dipilih
+                string[] files = Directory.GetFiles(selectedFolder);
+
+                // Menyusun daftar file untuk ditampilkan di textbox_edit
+                StringBuilder fileList = new StringBuilder();
+
+                foreach (string file in files)
+                {
+                    fileList.AppendLine(Path.GetFileName(file));
+                }
+
+                // Menampilkan daftar file di textbox_edit
+                if (fileList.Length > 0)
+                {
+                    text_edit.Text = fileList.ToString();
+                }
+                else
+                {
+                    text_edit.Text = "Folder kosong.";
+                }
+            }
+        }
+
+
+        private void simpan_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        currentFilePath = saveFileDialog.FileName;
+                    }
+                }
+            }
+            File.WriteAllText(currentFilePath, text_edit.Text);
+        }
+
+        private void hapus_Click(object sender, EventArgs e)
+        {
+            text_edit.Clear(); // Menghapus isi editor
+            currentFilePath = ""; // Reset currentFilePath untuk menunjukkan file baru
+        }
+
+        private void cari_Click(object sender, EventArgs e)
+        {
+            string searchTerm = text_cari.Text;
+            if (text_edit.Text.Contains(searchTerm))
+            {
+                MessageBox.Show("File ditemukan!");
+            }
+            else
+            {
+                MessageBox.Show("File tidak ditemukan.");
+            }
+        }
+    }
+}
